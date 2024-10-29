c915370cab23e3482815dd210424f55d3e8078e0
agungkusaeri9 <agung.kusaaeri9@gmail.com>
2024-10-29 21:31:35 +0700

    fix: configuration vite js

--- resources/views/components/frontend/head.blade.php
+++ resources/views/components/frontend/head.blade.php
@@ -3,5 +3,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="icon" href="img/favicon.png" type="image/png">
 <link rel="icon" href="{{ $setting->favicon() }}" type="image/png" sizes="16x16">
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
 @stack('styles')
